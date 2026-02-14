8420fe2bdb14e86629913c76fa5cd3da84683ac8
Divya Mer <divyamer066@gmail.com>
2026-02-14 17:47:12 +0530

    Add files via upload

---
+++ fahrenheit.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    float celsius, fahrenheit;
+    printf("Enter temperature in Celsius: ");
+    scanf("%f", &celsius);
+
+    fahrenheit = (1.8 * celsius) + 32;
+
+    printf("Temperature in Fahrenheit: %.2f", fahrenheit);
+    return 0;
+}
