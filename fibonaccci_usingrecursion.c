670a863040dc66790f1a9b507590858975bbf41d
Divyaa <divyamer066@gmail.com>
2026-05-08 20:55:56 +0530

    Create fibonaccci_usingrecursion.c

---
+++ fibonaccci_usingrecursion.c
@@ -0,0 +1,19 @@
+// fibonacci series using recursion
+
+#include<stdio.h>
+int fibonacci(int n){
+    if(n==0)
+    return 0;
+    if(n==1)
+    return 1;
+    return (n-1)+fibonacci(n-2);
+}
+int main(){
+    int i,n;
+    printf("enter number");
+    scanf("%d",&n);
+    for(i=0;i<n;i++){
+        printf("%d",fibonacci(i));
+    }
+    return 0;
+}
